5ed93c2a5cd11b0b5e33fe2cda9741a3c187c1cd
Brianel254 <86538128+Brianel254@users.noreply.github.com>
2025-06-13 03:35:37 +0300

    Update jep.js2

--- jep.js
+++ jep.js
@@ -1,14 +1,13 @@
+/* jep.js - JavaScript Email Protection (JEP) */
+
 function jep_link(domain, user) {
-    const email = `${user}@${domain}`;
-    const mailto = `mailto:${email}`;
+    // Create email string
+    let email = `${user}@${domain}`;
+    
+    // Obfuscate each character using HTML entity codes
+    let encodedEmail = "";
+    let encode = str => [...str].map(c => `&#${c.charCodeAt(0)};`).join('');
 
-    const encode = str => [...str].map(c => `&#${c.charCodeAt(0)};`).join('');
-
-    const a = document.createElement("a");
-    a.setAttribute("href", encode(mailto));
-    a.innerHTML = encode(email);
-
-    // Append to a target container (like the calling script's parent)
-    const scriptEl = document.currentScript;
-    scriptEl.parentNode.insertBefore(a, scriptEl.nextSibling);
+    // Insert the obfuscated email link into the DOM
+    document.write(`<a href="${encode(mailto)}">${encode(email)}</a>`);
 }
